183abcf4b5886af8c033990e5cc73b7a22cd4313
CooIDragon <90614965+CooIDragon@users.noreply.github.com>
2022-06-07 23:01:53 +0300

    Add files via upload

---
+++ src/ru/vsu/cs/baturin_v_a/Program.java
@@ -0,0 +1,35 @@
+package ru.vsu.cs.baturin_v_a;
+
+import java.awt.EventQueue;
+import static java.awt.Frame.MAXIMIZED_BOTH;
+import java.util.Locale;
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+import ru.vsu.cs.baturin_v_a.util.SwingUtils;
+
+
+public class Program {
+
+    /**
+     * Основная функция программы
+     *
+     * @param args Параметры командной строки
+     * @throws Exception Любое исключение
+     */
+    public static void main(String[] args) throws Exception {
+        Locale.setDefault(Locale.ROOT);
+
+        UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+        SwingUtils.setDefaultFont("Arial", 20);
+
+        EventQueue.invokeLater(() -> {
+            try {
+                JFrame frameMain = new TreeDemoFrame();
+                frameMain.setVisible(true);
+                frameMain.setExtendedState(MAXIMIZED_BOTH);
+            } catch (Exception ex) {
+                SwingUtils.showErrorMessageBox(ex);
+            }
+        });
+    }
+}
